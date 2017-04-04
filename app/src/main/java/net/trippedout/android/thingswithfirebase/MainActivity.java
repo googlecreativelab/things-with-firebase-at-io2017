24975eb50df92c623c07d4a03d7630208a719f1e
Anthony Tripaldi <atripaldi@google.com>
2017-04-04 13:19:55 -0400

    add other button refs and clean up

--- app/src/main/java/net/trippedout/android/thingswithfirebase/MainActivity.java
+++ app/src/main/java/net/trippedout/android/thingswithfirebase/MainActivity.java
@@ -1,16 +1,12 @@
 package net.trippedout.android.thingswithfirebase;
 
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.things.contrib.driver.adcv2x.Adcv2x;
-import com.google.android.things.pio.PeripheralManagerService;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.things.contrib.driver.button.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +15,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import net.trippedout.android.thingswithfirebase.listeners.SimpleValueListener;
 
 import java.io.IOException;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase mDatabase;
-    private DatabaseReference mDialRef;
+    private DatabaseReference mDialRef, mPrevRef, mNextRef;
+    private Button mPrevButton, mNextButton;
+    private Adcv2x mAdc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +57,23 @@ public class MainActivity extends AppCompatActivity {
         setupThings();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try {
+            if(mNextButton != null) {
+                mNextButton.close();
+            }
+
+            if(mAdc != null) {
+                mAdc.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void setupFirebase() {
         //TODO auth doesnt work yet with Things
 //        mAuth = FirebaseAuth.getInstance();
@@ -67,44 +81,52 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance();
         mDialRef = mDatabase.getReference("things/dial");
-
-        // Read from the database
-        mDialRef.addValueEventListener(new SimpleValueListener() {
-            @Override
-            public void onValue(DataSnapshot snap) {
-                Log.d(TAG, "Key is: " + snap.getKey() + " Value is: " + snap.getValue());
-            }
-
-            @Override
-            public void onChildValues(Iterable<DataSnapshot> children) {
-                for (DataSnapshot snap : children) {
-                    Log.d(TAG, "Key is: " + snap.getKey() + " Value is: " + snap.getValue());
-                }
-            }
-        });
+        mPrevRef = mDatabase.getReference("things/prev");
+        mNextRef = mDatabase.getReference("things/next");
     }
 
     private void setupThings() {
-        PeripheralManagerService manager = new PeripheralManagerService();
-        List<String> deviceList = manager.getI2cBusList();
-        if (deviceList.isEmpty()) {
-            Log.i(TAG, "No I2C bus available on this device.");
-        } else {
-            Log.i(TAG, "List of available devices: " + deviceList);
-        }
+        setupButtons();
+        setupDial();
+    }
 
+    private void setupButtons() {
         try {
-            final Adcv2x adc = new Adcv2x("I2C1");
+            mPrevButton = new Button("GP14", Button.LogicState.PRESSED_WHEN_HIGH);
+            mPrevButton.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean b) {
+                    mPrevRef.setValue(b);
+                }
+            });
+
+            mNextButton = new Button("GP15", Button.LogicState.PRESSED_WHEN_HIGH);
+            mNextButton.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean b) {
+                    mNextRef.setValue(b);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setupDial() {
+        try {
+            // Create the driver for our ADC board, needed for the analog signal from our pot
+            mAdc = new Adcv2x("I2C1");
 
             // set the range here to 4 volts even tho we know we're only feeding it ~3.3
             // the dial turned all the way up will read ~3.2
-            adc.setRange(Adcv2x._4_096V);
+            mAdc.setRange(Adcv2x._4_096V);
 
+            // loop through reads
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        float v = adc.getResult(0);
+                        float v = mAdc.getResult(0);
                         float diff = currentVoltage - v;
 
                         // only update database if we're beyond the previous difference
@@ -125,8 +147,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateVoltageInDatabase(float currentVoltage) {
-        Log.d(TAG, "updateVoltageInDatabase: " + currentVoltage);
-
         float percentage = currentVoltage / MAX_VOLTAGE_FROM_POT;
         if(percentage < 0.f) percentage = 0.f;
         if(percentage > 1.f) percentage = 1.f;
